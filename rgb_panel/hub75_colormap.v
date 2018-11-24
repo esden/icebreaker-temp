0da169fffd18e98d4e2835e7528e5109ea018b82
Sylvain Munaut <tnt@246tNt.com>
2018-11-24 11:11:39 +0100

    rgb_panel: Add non-trivial color mapper
    This accepts RGB24, RGB565 (16b) and RGB332 (8b) and applies gamma.
    
    Signed-off-by: Sylvain Munaut <tnt@246tNt.com>

--- rgb_panel/hub75_colormap.v
+++ rgb_panel/hub75_colormap.v
@@ -20,23 +20,110 @@ module hub75_colormap #(
 	input  wire [BITDEPTH-1:0] in_data,
 	input  wire [USER_WIDTH-1:0] in_user,
 	input  wire in_valid,
-	output wire in_ready,
+	output reg  in_ready,
 
 	// Output pixel
 	output wire [(N_CHANS*N_PLANES)-1:0] out_data,
 	output wire [USER_WIDTH-1:0] out_user,
-	output wire out_valid,
+	output reg  out_valid,
 
 	// Clock / Reset
 	input  wire clk,
 	input  wire rst
 );
+	// Signals
+	// -------
 
-	// Trivial mapping ATM
-	assign out_valid = in_valid;
-	assign out_data  = in_data;
-	assign out_user  = in_user;
+	wire [7:0] c0;
+	wire [7:0] c1;
+	wire [7:0] c2;
+	reg  [7:0] cmux;
 
-	assign in_ready  = 1'b1;
+	reg  [1:0] cnt;
+
+	wire [N_PLANES-1:0] do;
+	reg  [N_PLANES-1:0] do_r[0:1];
+
+
+	// Control
+	// -------
+
+		// Little note: Technically we need only 3 cycles to lookup the
+		// 3 colors. But to avoid having some registers to pipeline the 'user'
+		// data, we take 4 cycles. It doesn't matter anyway since we'll have
+		// to wait to pipe the data to the LCD anyway, so 'wasting' a cycle
+		// here has no consequence.
+
+	// Cycle counter
+	always @(posedge clk)
+		cnt <= in_valid ? (cnt + 1) : 2'b00;
+
+
+	// Input stage
+	// -----------
+
+	// Map color channels
+	generate
+		if (BITDEPTH == 24) begin
+			assign c2 = in_data[23:16];
+			assign c1 = in_data[15: 8];
+			assign c0 = in_data[ 7: 0];
+		end else if (BITDEPTH == 16) begin
+			assign c2 = { in_data[15:11], in_data[15:13] };
+			assign c1 = { in_data[10: 5], in_data[10: 9] };
+			assign c0 = { in_data[ 4: 0], in_data[ 4: 2] };
+		end else if (BITDEPTH == 8) begin
+			assign c2 = { {2{in_data[7:5]}}, in_data[7:6] };
+			assign c1 = { {2{in_data[4:2]}}, in_data[4:3] };
+			assign c0 = { {4{in_data[1:0]}} };
+		end
+	endgenerate
+
+	// Mux
+	always @(*)
+		case (cnt)
+			2'b00: cmux = c0;
+			2'b01: cmux = c1;
+			2'b10: cmux = c2;
+			default: cmux = 8'hx;
+		endcase
+
+	// When are we ready
+	always @(posedge clk)
+		in_ready <= (cnt == 2'b10);
+
+
+	// Gamma LUT
+	// ---------
+
+	hub75_gamma #(
+		.IW(8),
+		.OW(N_PLANES)
+	) gamma_lut_I (
+		.in(cmux),
+		.out(do),
+		.enable(1'b1),
+		.clk(clk)
+	);
+
+
+	// Output stage
+	// ------------
+
+	// Data
+	always @(posedge clk)
+	begin
+		do_r[1] <= do;
+		do_r[0] <= do_r[1];
+	end
+
+	assign out_data = { do, do_r[1], do_r[0] };
+
+	// User infos
+	assign out_user = in_user;
+
+	// Valid signal
+	always @(posedge clk)
+		out_valid <= (cnt == 2'b10);
 
 endmodule // hub75_colormap
---
+++ rgb_panel/hub75_gamma.v
@@ -0,0 +1,44 @@
+/*
+ * hub75_gamma.v
+ *
+ * CERN Open Hardware Licence v1.2 - See LICENSE
+ *
+ * Copyright (C) 2018  Sylvain Munaut <tnt@246tNt.com>
+ *
+ * vim: ts=4 sw=4
+ */
+
+`default_nettype none
+
+module hub75_gamma #(
+	parameter IW = 8,
+	parameter OW = 10
+)(
+	input  wire [IW-1:0] in,
+	output wire [OW-1:0] out,
+	input  wire enable,
+	input  wire clk
+);
+	reg  [15:0] gamma_rom [0:255];
+	wire [ 7:0] rd_addr;
+	reg  [15:0] rd_data;
+
+	initial
+		$readmemh("gamma_table.hex", gamma_rom);
+
+	always @(posedge clk)
+	begin
+		// Read
+		if (enable)
+			rd_data <= gamma_rom[rd_addr];
+	end
+
+	genvar i;
+	generate
+		for (i=0; i<8; i=i+1)
+			assign rd_addr[7-i] = in[IW-1-(i%IW)];
+	endgenerate
+
+	assign out = rd_data[15:16-OW];
+
+endmodule // hub75_gamma
